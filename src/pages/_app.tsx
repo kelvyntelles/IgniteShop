e54692822e595d7a28a17b466a58c5262cf08d38
kelvyntelles <kelvyn.telles@gmail.com>
2023-10-09 14:27:30 -0300

    projeto completo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+import type { AppProps } from 'next/app'
+
+import Image from 'next/image'
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg.src}
+          width={200}
+          height={200}
+          alt=""
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
